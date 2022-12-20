c9e794ec8cfde4d5e76b46fe66180e48151ab3ad
JJ Nuñez <jj@votepalmbeach.gov>
2022-12-20 12:15:30 -0500

    Stash progress on day 5

--- Day 05 - Supply Stacks/script.py
+++ Day 05 - Supply Stacks/script.py
@@ -6,6 +6,8 @@ Solution written by JJ Nunez.
 
 import sys
 
+# Debug function to show raw string list
+# that we read in from the file
 def _debugListContents(message, data):
 	print(message)
 	for line in data:
@@ -13,7 +15,8 @@ def _debugListContents(message, data):
 	print('================')
 	return
 
-
+# Function to read the input data and return
+# a list of the lines of the first half of it
 def _getCrateData(data):
 	crateData = []
 	for line in data:
@@ -22,6 +25,8 @@ def _getCrateData(data):
 		else:
 			return crateData
 
+# Function to find where the raw input data
+# is bisected
 def _getStartIndexForMoveData(data):
 	for index, line in enumerate(data):
 		if line != '':
@@ -29,6 +34,8 @@ def _getStartIndexForMoveData(data):
 		else:
 			return index+1	
 
+# Function to read the input data and return
+# a list of the lines of the second half of it
 def _getMoveData(data, startIndexForMoveData):
 	moveData = []
 	for index, line in enumerate(data):
@@ -38,24 +45,55 @@ def _getMoveData(data, startIndexForMoveData):
 			moveData.append(line)
 	return moveData
 
+# Function to get the number of stacks from the raw data
+def _getStackCount(crateData):
+	lengthOfLastLine = len(crateData[-1])
+	return int(''.join(crateData[-1][lengthOfLastLine-2]))
+
+# Function to prepare an empty stack data structure
 def _getEmptyListOfStacks(crateData):
 	# This function assumes that the second-to-last
 	# character of the last crate data line is a
 	# single-digit integer value
-	lengthOfLastLine = len(crateData[-1])
-	stackCount = int(''.join(crateData[lengthOfLastLine-2]))
+	stackCount = _getStackCount(crateData)
 	listOfStacks = []
 	for _ in range(stackCount):
 		stack = []
 		listOfStacks.append(stack)
 
+# Function to get a list of indices
+# for each stack column
+def _getStackColumnIndices(crateData):
+	line = crateData[-1]
+	indices = []
+	for index, character in line:
+		if ''.join(character).isdigit():
+			indices.append(index)
 
+# Function to get the stack at a given column index
+def _getStack(crateData, stackColumnIndex):
+	stack = []
+	for rowIndex, line in reversed(crateData):
+		if rowIndex == 0:
+			continue
+		stack.append(''.join(line[stackColumnIndex]))
+	print("DEBUG: stack at", rowIndex, stackColumnIndex, "is", stack)
+
+# Function to populate an empty stack data structure
+def _populateListOfStacks(crateData, emptyListOfStacks):
+	# We start from the bottom of the raw input data
+	# and move our way up.
+	# First determine the indices of each column ID.
+	listOfStacks = emptyListOfStacks.copy()	
+	stackColumnIndices = _getStackColumnIndices(crateData)
+	_getStack(crateData, stackColumnIndices[0])
 
 def solve1(data):
 	crateData = _getCrateData(data)
 	startIndexForMoveData = _getStartIndexForMoveData(data)
 	moveData = _getMoveData(data, startIndexForMoveData)
-	listOfStacks = _getEmptyListOfStacks(crateData)
+	emptyListOfStacks = _getEmptyListOfStacks(crateData)
+	listOfStacks = _populateListOfStacks(crateData, emptyListOfStacks)
 
 	return
 
