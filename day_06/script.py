a8c4940e4513311f92bb695921b7736db8ccbde6
JJ Nunez <jjbnunez97@gmail.com>
2023-03-13 17:58:52 -0400

    Solve day 6

--- day_06/script.py
+++ day_06/script.py
@@ -10,14 +10,52 @@ import os
 """
 Helper functions
 """
+def _allFourCharsAreUnique(first, second, third, fourth):
+	if first == second:
+		return False
+	if first == third:
+		return False
+	if first == fourth:
+		return False
+	if second == third:
+		return False
+	if second == fourth:
+		return False
+	if third == fourth:
+		return False
+	return True
 
 """
 Solver functions
 """
 def _solve1(data):
-	return
+	for line in data:
+		for index, character in enumerate(line):
+			if index >= 3:
+				first = line[index-3]
+				second = line[index-2]
+				third = line[index-1]
+				fourth = character
+				if _allFourCharsAreUnique(first, second, third, fourth):
+					print('Characters processed before start-of-packet marker:', index+1)	
+					break
 
 def _solve2(data):
+	bag = set()
+
+	for line in data:
+		for index in range(0, len(line)-14+1):
+			substring = line[index:14+index]
+			for character in substring:
+				bag.add(character)
+			if len(bag) == 14:
+				print('First marker after character ', index+14)
+				bag.clear()
+				break
+			else:
+				bag.clear()
+				continue
+	
 	return
 
 """
