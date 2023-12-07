93847a1727757f7042feb0b30573ce1b122b07cb
JJ Nunez <jj@votepalmbeach.gov>
2023-12-07 16:18:23 -0500

    Stash progress for day 1

--- day_01/script.py
+++ day_01/script.py
@@ -7,15 +7,64 @@ Solution written by JJ Nunez.
 
 import os
 
-# Solver function
+
+# Helper functions
+dictNumericCharacters = {
+    '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+    '6': 6, '7': 7, '8': 8, '9': 9, '0': 0,
+}
+
+dictNumericStrings = {
+    'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+    'six': 6, 'seven':7, 'eight': 8, 'nine': 9, 'zero': 0,
+}
+
+dictNumericStringsReversed = {
+    'eno': 1, 'owt': 2, 'eerht': 3, 'ruof': 4, 'evif': 5,
+    'xis': 6, 'neves': 7, 'thgie': 8, 'enin': 9, 'orez':0,
+}
+
+def _getFirstDigit(datum):
+    for character in datum:
+        if character.isdigit():
+            return character
+
+def _getFirstDigit2(datum):
+    lowestNumericString = ''
+    lowestIndex = -1
+    for string in dictNumericStrings.items():
+		index = datum.find
+		if datum.find(string) != -1:
+			if lowestIndex == -1:
+
+# Solver functions
 
 
-def _solve(data):
-    return
+def _solve1(data):
+    digitPairs = []
+    for datum in data:
+        pair = []
+        pair.append(_getFirstDigit(datum))
+        pair.append(_getFirstDigit(reversed(datum)))
+        digitPairs.append(pair)
+    values = []
+    for pair in digitPairs:
+        value = int(pair[0] + pair[1])
+        values.append(value)
+    sum = 0
+    for value in values:
+        sum += value
+    print(sum)
+
+
+def _solve2(data):
+
+	datum = data[0]
+	numericStringIndices = []
+	for string in numericStrings:
+        
 
 # Execution and File I/O
-
-
 def _readFile(fileName):
     scriptDirectory = os.path.dirname(__file__)
     filePath = os.path.join(scriptDirectory, fileName)
@@ -34,10 +83,10 @@ def main():
     inputData = _readFile(inputFileName)
 
     print(sampleFileName)
-    _solve(sampleData)
+    _solve1(sampleData)
 
     print(inputFileName)
-    _solve(inputData)
+    _solve1(inputData)
 
 
 # Allows execution only from command line
