7e5ed66c30a7a9a981b40ae24ee351ac2ba2fe4c
JJ Nunez <jjbnunez97@gmail.com>
2024-12-03 11:12:41 -0500

    Solve day 1 part 1

---
+++ day_01/script.py
@@ -0,0 +1,82 @@
+"""
+Advent of Code 2024
+Day 1: Historian Hysteria
+
+Solution written by JJ Nunez.
+"""
+
+import os
+
+
+# Helper functions and classes
+class Location:
+    def __init__(self, id):
+        self.id = id
+
+# Solver functions
+
+
+def _solve1(data):
+    # Create two empty lists
+    list0 = []
+    list1 = []
+
+    # Populate the lists with unsorted data
+    for datum in data:
+        pair = datum.split("   ")
+        list0.append(pair[0])
+        list1.append(pair[1])
+
+    # Sort the lists
+    list0.sort()
+    list1.sort()
+
+    # Create an empty "distances" list
+    distances = []
+
+    # Iterate over the two lists and calculate distances
+    for item0, item1 in zip(list0, list1):
+        distance = int(item0) - int(item1)
+        if distance < 0:
+            distance = 0 - distance
+        distances.append(distance)
+
+    # Display the sum
+    print(sum(distances))
+
+
+def _solve2(data):
+    datum = data[0]
+    numericStringIndices = []
+
+# Execution and File I/O
+
+
+def _readFile(fileName):
+    scriptDirectory = os.path.dirname(__file__)
+    filePath = os.path.join(scriptDirectory, fileName)
+    with open(filePath, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    data = []
+    for line in lines:
+        data.append(line.replace('\n', ''))
+    return data
+
+
+def main():
+    sampleFileName = 'sample.txt'
+    inputFileName = 'input.txt'
+    sampleData = _readFile(sampleFileName)
+    inputData = _readFile(inputFileName)
+
+    print(sampleFileName)
+    _solve1(sampleData)
+
+    print(inputFileName)
+    _solve1(inputData)
+
+
+# Allows execution only from command line
+# and not from import statements
+if __name__ == '__main__':
+    main()
