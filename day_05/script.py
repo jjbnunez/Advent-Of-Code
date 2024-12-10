d83ffb1580e47bc2cb4692363cac27bc66e22e95
JJ Nunez <jjbnunez97@gmail.com>
2024-12-10 17:03:11 -0500

    Set up workspace for day 5

---
+++ day_05/script.py
@@ -0,0 +1,71 @@
+"""
+Advent of Code 2024
+Day 5: Print Queue
+
+Solution written by JJ Nunez.
+"""
+
+import os
+from copy import deepcopy
+
+################################################
+#                                              #
+#         Helper functions and classes         #
+#                                              #
+################################################
+
+
+def _helper(data):
+    return True
+
+################################################
+#                                              #
+#               Solver functions               #
+#                                              #
+################################################
+
+
+def _solve1(data):
+    data_copy = deepcopy(data)
+
+
+def _solve2(data):
+    data_copy = deepcopy(data)
+
+################################################
+#                                              #
+#            Execution and File I/O            #
+#                                              #
+################################################
+
+
+def _readFile(fileName):
+    scriptDirectory = os.path.dirname(__file__)
+    filePath = os.path.join(scriptDirectory, fileName)
+    with open(filePath, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    data = []
+    for line in lines:
+        data.append(line.replace('\n', ''))
+    return data
+
+
+def main():
+    sampleFileName = 'sample.txt'
+    inputFileName = 'input.txt'
+    sampleData = _readFile(sampleFileName)
+    inputData = _readFile(inputFileName)
+
+    print(sampleFileName)
+    _solve1(sampleData)
+    _solve2(sampleData)
+
+    print(inputFileName)
+    _solve1(inputData)
+    _solve2(inputData)
+
+
+# Allows execution only from command line
+# and not from import statements
+if __name__ == '__main__':
+    main()
