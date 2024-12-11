144c9615057db4dce9eca8d536f296cc12054be2
JJ Nunez <jjbnunez97@gmail.com>
2024-12-11 16:55:09 -0500

    Stash progress for day 5 part 1

--- day_05/script.py
+++ day_05/script.py
@@ -15,8 +15,55 @@ from copy import deepcopy
 ################################################
 
 
-def _helper(data):
-    return True
+def _print_lines(data: list[str]):
+    for line in data:
+        print(line)
+
+
+def _get_ordering_rule_lines(data: list[str]):
+    ordering_rule_lines: list[str] = []
+    for line in data:
+        if line == '':
+            break
+        else:
+            ordering_rule_lines.append(line)
+    return ordering_rule_lines
+
+
+def _get_page_number_lines(data: list[str]):
+    page_number_lines: list[str] = []
+    encountered_yet: bool = False
+    for line in data:
+        if line == '':
+            encountered_yet = True
+            continue
+        elif encountered_yet == False:
+            continue
+        else:
+            page_number_lines.append(line)
+    return page_number_lines
+
+
+def _get_ordering_rule_array(lines: list[str]):
+    ordering_rules: list[list[int]] = []
+    for line in lines:
+        split_string = line.split("|")
+        pair: list[int] = []
+        for item in split_string:
+            pair.append(int(item))
+        ordering_rules.append(pair)
+    return ordering_rules
+
+
+def _get_page_number_array(lines: list[str]):
+    page_numbers: list[list[int]] = []
+    for line in lines:
+        split_string = line.split(",")
+        group: list[int] = []
+        for item in split_string:
+            group.append(int(item))
+        page_numbers.append(group)
+    return page_numbers
 
 ################################################
 #                                              #
@@ -25,11 +72,34 @@ def _helper(data):
 ################################################
 
 
-def _solve1(data):
+def _solve1(data: list[str]):
+
+    # Process the data into structures
     data_copy = deepcopy(data)
+    ordering_rule_lines = _get_ordering_rule_lines(data_copy)
+    page_number_lines = _get_page_number_lines(data_copy)
+    ordering_rules = _get_ordering_rule_array(ordering_rule_lines)
+    page_numbers = _get_page_number_array(page_number_lines)
+
+    # I am considering an associative array here
+    before_dictionary: dict = {}
+    after_dictionary: dict = {}
+    for pair in ordering_rules:
+        if before_dictionary.get(pair[1]) == None:
+            before_dictionary[pair[1]] = set()
+        if after_dictionary.get(pair[0]) == None:
+            after_dictionary[pair[0]] = set()
+        before_dictionary[pair[1]].add(pair[0])
+        after_dictionary[pair[0]].add(pair[1])
+    print("BEFORE_DICTIONARY")
+    for index, (key, value) in enumerate(before_dictionary.items()):
+        print(f"Index: {index}, Key: {key}, Value: {value}")
+    print("AFTER_DICTIONARY")
+    for index, (key, value) in enumerate(after_dictionary.items()):
+        print(f"Index: {index}, Key: {key}, Value: {value}")
 
 
-def _solve2(data):
+def _solve2(data: list[str]):
     data_copy = deepcopy(data)
 
 ################################################
@@ -39,10 +109,10 @@ def _solve2(data):
 ################################################
 
 
-def _readFile(fileName):
-    scriptDirectory = os.path.dirname(__file__)
-    filePath = os.path.join(scriptDirectory, fileName)
-    with open(filePath, 'r', encoding='utf-8') as file:
+def _read_file(file_name):
+    script_directory = os.path.dirname(__file__)
+    file_path = os.path.join(script_directory, file_name)
+    with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
     data = []
     for line in lines:
@@ -51,18 +121,18 @@ def _readFile(fileName):
 
 
 def main():
-    sampleFileName = 'sample.txt'
-    inputFileName = 'input.txt'
-    sampleData = _readFile(sampleFileName)
-    inputData = _readFile(inputFileName)
+    sample_file_name = 'sample.txt'
+    input_file_name = 'input.txt'
+    sample_data = _read_file(sample_file_name)
+    input_data = _read_file(input_file_name)
 
-    print(sampleFileName)
-    _solve1(sampleData)
-    _solve2(sampleData)
+    print(sample_file_name)
+    _solve1(sample_data)
+    # _solve2(sample_data)
 
-    print(inputFileName)
-    _solve1(inputData)
-    _solve2(inputData)
+    print(input_file_name)
+    # _solve1(input_data)
+    # _solve2(input_data)
 
 
 # Allows execution only from command line
